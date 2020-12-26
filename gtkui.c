2328cea87691cd44148155bb55e966c394fc4f62
Florian3k <24961583+Florian3k@users.noreply.github.com>
2020-12-26 12:51:42 +0100

    fix style

--- gtkui.c
+++ gtkui.c
@@ -3,26 +3,25 @@
 UI UICreateWindow(const char* title, const WindowConfig cfg) {
     UI window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), title);
-    gtk_window_set_default_size(GTK_WINDOW(window), 1020, 720);
-    gtk_window_set_resizable (GTK_WINDOW(window), cfg.width);
-    if (cfg.center) gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit),NULL);
+    gtk_window_set_default_size(GTK_WINDOW(window), cfg.width, cfg.height);
+    gtk_window_set_resizable(GTK_WINDOW(window), cfg.resizable);
+    if (cfg.center) {
+        gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    }
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_widget_show(window);
     return window;
 }
 
 void UILoadCSS(const char* filename) {
     static bool notry = true;
-    GtkCssProvider *provider;
-    GdkDisplay *display;
-    GdkScreen *screen;
-    provider = gtk_css_provider_new();
-    display = gdk_display_get_default();
-    screen = gdk_display_get_default_screen(display);
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    GError *error = 0;
+    GtkCssProvider* provider = gtk_css_provider_new();
+    GdkDisplay* display = gdk_display_get_default();
+    GdkScreen* screen = gdk_display_get_default_screen(display);
+    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    GError* error = 0;
     gtk_css_provider_load_from_file(provider, g_file_new_for_path(filename), &error);
-    g_object_unref (provider);
+    g_object_unref(provider);
     if (error && notry) {
         char result[1024];
         ssize_t count = readlink("/proc/self/exe", result, 1024);
@@ -35,16 +34,16 @@ void UILoadCSS(const char* filename) {
 }
 
 void UILoadCSSData(const char* data) {
-    GtkCssProvider *provider;
-    GdkDisplay *display;
-    GdkScreen *screen;
+    GtkCssProvider* provider;
+    GdkDisplay* display;
+    GdkScreen* screen;
     provider = gtk_css_provider_new();
     display = gdk_display_get_default();
     screen = gdk_display_get_default_screen(display);
-    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-    GError *error = NULL;
+    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    GError* error = NULL;
     gtk_css_provider_load_from_data(provider, data, strlen(data), &error);
-    g_object_unref (provider);
+    g_object_unref(provider);
 }
 
 void UIAppend(UI parent, UI current) {
@@ -57,20 +56,17 @@ void UISetID(UI widget, const char* name) {
 }
 
 void UIAddClass(UI widget, const char* name) {
-    GtkStyleContext *ctx;
-    ctx = gtk_widget_get_style_context(widget);
+    GtkStyleContext* ctx = gtk_widget_get_style_context(widget);
     gtk_style_context_add_class(ctx, name);
 }
 
 bool UIHasClass(UI widget, const char* name) {
-    GtkStyleContext *ctx;
-    ctx = gtk_widget_get_style_context(widget);
+    GtkStyleContext* ctx = gtk_widget_get_style_context(widget);
     return gtk_style_context_has_class(ctx, name);
 }
 
 void UIRemoveClass(UI widget, const char* name) {
-    GtkStyleContext *ctx;
-    ctx = gtk_widget_get_style_context(widget);
+    GtkStyleContext* ctx = gtk_widget_get_style_context(widget);
     gtk_style_context_remove_class(ctx, name);
 }
 
@@ -100,23 +96,25 @@ void UIAttach(UI grid, UI child, int x, int y, int width, int height) {
 
 UI UICreateBox(char orientation) {
     UI box = NULL;
-    switch(orientation) {
+    switch (orientation) {
         case '-':
-        box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-        break;
+            box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+            break;
         case '|':
-        box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-        break;
+            box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+            break;
         default:
-        box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-        break;
+            box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+            break;
     }
     return box;
 }
 
 UI UICreateButton(const char* title, void* action) {
     UI button = gtk_button_new_with_mnemonic(title);
-    if (action != NULL) g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(action), NULL);
+    if (action != NULL) {
+        g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(action), NULL);
+    }
     return button;
 }
 
@@ -126,39 +124,39 @@ UI UICreateLabel(const char* text) {
 
 void UIError(const char* message) {
     UI dialog = gtk_message_dialog_new(
-        NULL, 
-        GTK_DIALOG_MODAL, 
-        GTK_MESSAGE_ERROR, 
+        NULL,
+        GTK_DIALOG_MODAL,
+        GTK_MESSAGE_ERROR,
         GTK_BUTTONS_OK,
         "%s",
         message
     );
-    gtk_dialog_run (GTK_DIALOG (dialog));
-    gtk_widget_destroy (dialog);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
 }
 
 void UIWarning(const char* message) {
     UI dialog = gtk_message_dialog_new(
-        NULL, 
-        GTK_DIALOG_MODAL, 
-        GTK_MESSAGE_WARNING, 
+        NULL,
+        GTK_DIALOG_MODAL,
+        GTK_MESSAGE_WARNING,
         GTK_BUTTONS_OK,
         "%s",
         message
     );
-    gtk_dialog_run (GTK_DIALOG (dialog));
-    gtk_widget_destroy (dialog);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
 }
 
 void UIPrint(const char* message) {
     UI dialog = gtk_message_dialog_new(
-        NULL, 
-        GTK_DIALOG_MODAL, 
-        GTK_MESSAGE_INFO, 
+        NULL,
+        GTK_DIALOG_MODAL,
+        GTK_MESSAGE_INFO,
         GTK_BUTTONS_OK,
         "%s",
         message
     );
-    gtk_dialog_run (GTK_DIALOG (dialog));
-    gtk_widget_destroy (dialog);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
 }
